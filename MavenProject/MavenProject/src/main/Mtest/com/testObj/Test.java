287bde1933c5269857d3a66efb4239b3cd9b4c67
Chandler Lagarde <clag1995@yahoo.com>
2017-08-02 11:07:07 -0500

    Add files via upload

---
+++ MavenProject/MavenProject/src/main/Mtest/com/testObj/Test.java
@@ -0,0 +1,33 @@
+package com.testObj;
+
+import java.io.*;
+import java.util.*;
+import com.Mbean.EmployeeLoginAccount;
+
+public class Test {
+
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(isr);
+    
+//	String inputUser= br.readLine();
+//  String inputPassword = br.readLine();
+    
+    Test t = new Test();
+//  t.readPassword(inputUser, inputPassword);
+
+	public void readPassword(String inputUser,String inputPassword) throws Exception {
+//		absolute path
+		Scanner scr = new Scanner(new File("C:/Users/ChandlerLagarde/login_account.csv"));
+		boolean flag=false;
+		while(scr.hasNextLine()){
+			String line = scr.nextLine();
+			String username= line.substring(0,line.indexOf(' '));
+			String password =line.substring(line.indexOf(' ')+1,line.length());
+			System.out.println(username+","+password);
+		}   
+	}
+}
+
+
+
+
